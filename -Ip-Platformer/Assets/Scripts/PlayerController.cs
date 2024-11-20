92d00bafcf693b9470475a7229e852af2781c5a6
moonicecreamstudio <moonicecreamstudio@gmail.com>
2024-11-20 12:18:57 -0500

    Movement left and right added

--- -Ip-Platformer/Assets/Scripts/PlayerController.cs
+++ -Ip-Platformer/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     public enum FacingDirection
     {
         left, right
     }
 
+    public float timeToReachMaxSpeed;
+    public float maxSpeed;
+    public float timeToDecelerate;
+
+    public float acceleration;
+    public float deceleration;
+    private Rigidbody2D playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        acceleration = maxSpeed / timeToReachMaxSpeed;
+        deceleration = maxSpeed / timeToDecelerate;
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,13 +31,43 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         MovementUpdate(playerInput);
+        Debug.Log("playerInput = " + playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        // Easier to manage compared to addForce
 
+        Vector2 currentVelocity = playerRB.velocity;
+
+        // Move the character to the left
+        if (playerInput.x < 0)
+        {
+            // Do stuff to currentVelocity
+            currentVelocity += acceleration * Vector2.left * Time.deltaTime;
+        }
+
+        // Move the character to the right
+        if (playerInput.x > 0)
+        {
+            // Do stuff to currentVelocity
+            currentVelocity += acceleration * Vector2.right * Time.deltaTime;
+        }
+
+        // Move the character to the jumps
+        // Do stuff to currentVelocity
+
+        // If the character is currently accelerating:
+        if (playerInput.x == 0)
+        {
+            // Deaccelerate
+            currentVelocity = Vector2.zero;
+        }
+
+        //RB handles the delta time stuff
+        playerRB.velocity = currentVelocity;
     }
 
     public bool IsWalking()
